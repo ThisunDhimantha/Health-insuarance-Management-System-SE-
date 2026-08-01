55d934f562efb108c86fcccdfe26ee42d3c13189
Thisun Dhimantha <thisun989@gmail.com>
2026-08-01 14:51:52 +0530

    Delete src/main directory

--- src/main/java/com/insurance/system/repository/PolicyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.insurance.system.repository;
-
-import com.insurance.system.model.Policy;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PolicyRepository extends JpaRepository<Policy, Long> {
-}
